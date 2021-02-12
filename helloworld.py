e65ad63fa814345eb6dfee87f5117cb4d279b283
adh182 <adh.isl.almrjl@gmail.com>
2021-02-12 22:01:50 +0800

    Added born year.

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,7 @@ age = input("Enter your age here: ")
 
 print("Hello World")
 print("Hello" + name + ". Your age is " + age)
+
+current_year = 2021
+born = current_year - age
+print("You were born in " + born + " or " + born+1)
