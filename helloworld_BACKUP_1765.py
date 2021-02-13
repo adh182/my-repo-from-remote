05c494875b526a5c81fc18dda968620315474346
adh182 <adh.isl.almrjl@gmail.com>
2021-02-13 10:17:09 +0800

    Merge branch 'development-1.0' into education
    # Conflicts:
    #	helloworld.py

---
+++ helloworld_BACKUP_1765.py
@@ -0,0 +1,17 @@
+name = input("Enter your name here: ")
+age = input("Enter your age here: ")
+
+print("Hello World")
+print("Hello" + name + ". Your age is " + age)
+
+current_year = 2021
+born = current_year - age
+print("You were born in " + born + " or " + born+1)
+
+<<<<<<< HEAD
+education = input("Enter your education: ")
+print("Your edcation is : ", education)
+=======
+country = input("Enter your country : ")
+print("Your country is : ", country)
+>>>>>>> development-1.0
---
+++ helloworld_LOCAL_1765.py
@@ -0,0 +1,12 @@
+name = input("Enter your name here: ")
+age = input("Enter your age here: ")
+
+print("Hello World")
+print("Hello" + name + ". Your age is " + age)
+
+current_year = 2021
+born = current_year - age
+print("You were born in " + born + " or " + born+1)
+
+education = input("Enter your education: ")
+print("Your edcation is : ", education)
